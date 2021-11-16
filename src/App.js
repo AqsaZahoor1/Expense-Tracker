0f8e0fedea6b61259d1c7398641be5320614153b
Aqsa <aqsazahoor555@gmail.com>
2021-11-16 10:00:22 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
+
+  const expenses = [
+    {
+      id: "e1",
+      title:"Car Insurrance",
+      amount: 249.6, 
+      date:new Date(2021, 2 , 28)
+    },
+    {
+      id: "e2",
+      title:"Toilet Paper",
+      amount: 349.6, 
+      date:new Date(2021, 5 , 28)
+    },
+    {
+      id: "e3",
+      title:"New Tv",
+      amount: 449.6, 
+      date:new Date(2021, 6 , 28)
+    },
+    {
+      id: "e4",
+      title:"New desk Wooden",
+      amount: 549.6, 
+      date:new Date(2021, 6 , 28)
+    }
+  ];
+
   return (
-    <div className="App">
-      <h2>Let's get started Woo Hoo Hoo</h2>
+    <div >
+      <Expenses items = {expenses} />
     </div>
   );
 }
